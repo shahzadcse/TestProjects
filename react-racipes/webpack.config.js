94f7fc8c6f775992d905504947632e62192b2061
Shahzad Hussain <shahzadcse@gmail.com>
2019-07-16 14:48:03 +0400

    Add webpack config

---
+++ react-racipes/webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry : "./index.js",
+    output: {
+        path : __dirname,
+        filename: './dist/main.js'
+    },
+
+    module : {
+        rules : [
+            {
+                test : /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+
+};
