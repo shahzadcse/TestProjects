c502b7f06cb89bf07d9a1e3ad2ad0f8ace64f9cf
Shahzad Hussain <shahzadcse@gmail.com>
2019-07-17 17:35:52 +0400

    Lint configuration and fixing errors

--- react-recipes/src/components/App.js
+++ react-recipes/src/components/App.js
@@ -1,82 +1,53 @@
 import React from 'react';
-import Header from  './Header.js';
-import RecipeList from  './RecipeList.js';
-import RecipeDetails from  './RecipeDetails.js';
+import Header from './Header';
+import RecipeList from './RecipeList';
+import RecipeDetail from './RecipeDetail';
 
-
-
-
-
-{/*    Class component */}
 class App extends React.Component {
-
-    constructor(props){
+    constructor(props) {
         super(props);
-
         this.state = {
-            recipes : [],
-            currentRecipe : null,
+            recipes: [],
+            currentRecipe: null,
         };
-
-        {/* difference between Functional and class component. This line is for normal component */}
-       {/*this.onRecipeClick = this.onRecipeClick.bind(this);*/}
     }
 
     componentDidMount() {
         fetch(`${API_URL}/v1/recipes`)
-        .then(res => res.json())
-        .then(recipes => {
-            this.setState({ recipes });
-        });
+            .then(res => res.json())
+            .then((recipes) => {
+                this.setState({ recipes });
+            });
     }
-    
+
     onRecipeClick = (id) => {
         fetch(`${API_URL}/v1/recipes/${id}`)
-        .then(res => res.json())
-        .then(recipe => {
-            this.setState({ currentRecipe : recipe });
-        });
+            .then(res => res.json())
+            .then((recipe) => {
+                this.setState({ currentRecipe: recipe });
+            });
     }
-    
-    render() {
 
-        const {recipes, currentRecipe} = this.state;
-        return (
-            <div> 
-        <Header />
-        <main className="flex px4" >
-        <RecipeList 
-        recipes={recipes}
-        onClick={this.onRecipeClick}
-        className="pl4" 
-        style={{flex:3}}
-        />
-        <RecipeDetails 
-        className="ml4" 
-        recipe={currentRecipe} 
-        style={{flex:5}}
-        />    
-        </main>
-                 
-   </div>
+    render() {
+                const { recipes, currentRecipe } = this.state;
+                return (
+                <div>
+                    <Header />
+                    <main className="flex px4">
+                        <RecipeList
+                            recipes={recipes}
+                            onClick={this.onRecipeClick}
+                            className="pl4"
+                            style={{ flex: 3 }}
+                        />
+                        <RecipeDetail
+                            className="ml4"
+                            recipe={currentRecipe}
+                            style={{ flex: 5 }}
+                        />
+                    </main>
+                </div>
         );
     }
-
 }
-
-
-
-{/* functional component  
-const App = () => (
-    <div> 
-        <Header />
-        <main style={{display:'flex'}} >
-        <RecipeList style={{flex:3}}/>
-        <RecipeDetails style={{flex:5}}/>    
-        </main>
-                 
-   </div>
-);
-*/}
-
 export default App;
--- react-recipes/src/components/RecipeList.js
+++ react-recipes/src/components/RecipeList.js
@@ -1,23 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RecipeList = (props) => (
+const RecipeList = props => (
     <div style={props.style}>
        <h2 className="h2"> Recipes</h2>
        <ul className="list-reset">
-           {props.recipes.map( recipe => (
-               <li 
-               className="py2 border-bottom border-bottom-dotted pointer"
-               key={recipe.id}
-               onClick={()=> props.onClick(recipe.id)}              
-               >
-               <span>{recipe.name}</span>
-               <span>{recipe.category}</span>
-           </li>
+           {props.recipe.map(recipe => (
+                <li
+                    className="py2 border-bottom border-bottom-dotted pointer"
+                    key={recipe.id}
+                    onClick={() => props.onClick(recipe.id)}
+                >
+                    <span>{recipe.name}</span>
+                    <span>{recipe.category}</span>
+                </li>
            ))}
-            
-
        </ul>
     </div>
 );
+
+RecipeList.propTypes = {
+    style: PropTypes.object,
+    recipe: PropTypes.object,
+    onClick: PropTypes.func,
+};
 
 export default RecipeList;
