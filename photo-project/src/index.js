921f5f5796757ac159703bf4655142ea7ab7d617
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-28 20:40:19 +0400

    initial-commit Hello World

---
+++ photo-project/src/index.js
@@ -0,0 +1,7 @@
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+
+ const element  = React.createElement("h1", null, "Hello world");
+
+
+ ReactDOM.render(element, document.getElementById('root'))
