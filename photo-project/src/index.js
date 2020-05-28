0c7815fcc9d25784c5ff2f72503fd1f766767002
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-28 20:49:44 +0400

    Creating element in react

--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -1,7 +1,11 @@
- import React from 'react'
- import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
- const element  = React.createElement("h1", null, "Hello world");
+const element = React.createElement("ol", null,
+    React.createElement("li", null, "Get up early in the morning"),
+    React.createElement("li", null, "Go to Gym"),
+    React.createElement("li", null, "Start your day")    
+);
 
 
- ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(element, document.getElementById('root'))
