61994c2c9d6099f4ea73a4860fbb61aee16bddc3
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-28 22:46:39 +0400

    adding CSS

---
+++ photo-project/src/components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Title from './Title'
+ 
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Title title={"Photo App"} /> 
+            </div>
+        );
+    }
+}
+
+export default Main
--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -1,37 +1,8 @@
-import React, { Component } from 'react'
+import React  from 'react'
 import ReactDOM from 'react-dom'
-//creating array for elements
-//const tasks = ["Get up early in the morning", "Go to Gym", "Start your day"]
-  
-class Title extends Component {
-    render() {
-        return (
-            <h1>Task List</h1>
-        );
-    }
-}
+import Main from './components/Main' 
+import './styles/stylesheet.css'
 
-class Task extends Component {
-    render() {
-        return (
-            <ol>
-                {this.props.tasks.map((task, index) => <li key={index}>{task}</li>)}
-            </ol>
-        )
-    }
-}
 
-class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Title />
-                <Task tasks={["Get up early in the morning", "Go to Gym"]} /> 
-                <Task tasks={["Got to Gym", "Start your Work"]} /> 
-
-            </div>
-        );
-    }
-}
 
 ReactDOM.render(<Main />, document.getElementById('root'))
