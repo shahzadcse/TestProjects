90446fe592c9c5e39002c677cfd9136cd0d76c64
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-28 21:14:48 +0400

    breaking list into components

--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 //creating array for elements
 const tasks = ["Get up early in the morning", "Go to Gym", "Start your day"]
+  
+class Title extends Component {
+    render() {
+        return (
+            <h1>Task List</h1>
+        );
+    }
+}
 
-const element =
-    <div>
-        <h1>Task List</h1>
-    <ol>
-        {tasks.map((task, index) => <li key={index}>{task}</li>)}
-    </ol>
-    </div>
+class Task extends Component {
+    render() {
+        return (
+            <ol>
+                {tasks.map((task, index) => <li key={index}>{task}</li>)}
+            </ol>
+        )
+    }
+}
 
-ReactDOM.render(element, document.getElementById('root'))
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Title />
+                <Task />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
