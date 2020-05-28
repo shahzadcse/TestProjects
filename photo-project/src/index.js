b3e9a1157433d8973ddf9df4eb6857b6d621dfb0
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-28 21:05:32 +0400

    creating element in JSX

--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 //creating array for elements
-const tasks = ["Get up early in the morning", "Go to Gym","Start your day"  ]
+const tasks = ["Get up early in the morning", "Go to Gym", "Start your day"]
 
-const element = React.createElement("ol", null,
-    (tasks).map( ( task ) => React.createElement("li", null, task)  )
-);
+const element =
+
+    <ol>
+        {tasks.map((task, index) => <li key={index}>{task}</li>)}
+    </ol>
 
 
 ReactDOM.render(element, document.getElementById('root'))
