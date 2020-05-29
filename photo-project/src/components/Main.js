c2aa463a895b3d4820e502c1cf5d1ec728ae47fb
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-29 17:06:26 +0400

    simulate DB from external fetch and Component life Cycle

--- photo-project/src/components/Main.js
+++ photo-project/src/components/Main.js
@@ -7,29 +7,23 @@ import Title from './Title'
 class Main extends Component {
 
     constructor(){
+        console.log("constructor");
         super()
         this.state = {
-            allPhotos : [{
-                id: "0",
-                description: "beautiful sea",
-                imageLink: "https://picsum.photos/800/600?image=14"
-                }, {
-                id: "1",
-                description: "Coffee cup",
-                imageLink: "https://picsum.photos/800/600?image=30"
-                }, {
-                id: "2",
-                description: "Freedom",
-                imageLink: "https://picsum.photos/800/600?image=50"
-                }, {
-                id: "3",
-                description: "Farm",
-                imageLink: "https://picsum.photos/800/600?image=85"
-                }]
+            allPhotos : [] 
         }
         this.removePhotos = this.removePhotos.bind(this);
     } 
     
+    componentDidMount() {
+        console.log("componentDidMount");
+    const data =  SimulateDB();
+      console.log(data);
+      this.setState({
+          allPhotos : data
+      })
+    }
+
     removePhotos(photoremoved) {
         console.log(photoremoved.description);
         this.setState( (state) => ({
@@ -38,6 +32,7 @@ class Main extends Component {
     }
 
     render() {
+        console.log("render");
         return (
             <div>
                 <Title title={"Photo App"} /> 
@@ -45,6 +40,28 @@ class Main extends Component {
             </div>
         );
     }
+}
+
+function SimulateDB() {
+    return(
+        [{
+        id: "0",
+        description: "beautiful sea",
+        imageLink: "https://picsum.photos/800/600?image=14"
+        }, {
+        id: "1",
+        description: "Coffee cup",
+        imageLink: "https://picsum.photos/800/600?image=30"
+        }, {
+        id: "2",
+        description: "Freedom",
+        imageLink: "https://picsum.photos/800/600?image=50"
+        }, {
+        id: "3",
+        description: "Farm",
+        imageLink: "https://picsum.photos/800/600?image=85"
+        }]
+    );
 }
 
 export default Main
