a667544eff82cfbb52a3e7d494bd1c0149a4fb79
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-31 15:34:15 +0400

    Redux : creating reducer and data store

--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 import Main from './components/Main' 
 import './styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import rootReducer from './redux/reducer'
+
+const store =  createStore(rootReducer)
 
 
 ReactDOM.render(<BrowserRouter><Main /></BrowserRouter>, document.getElementById('root'))
---
+++ photo-project/src/redux/reducer.js
@@ -0,0 +1,7 @@
+import allPhotos from '../data/allPhotos'
+
+const allPhotosReducer = function(state = allPhotos , action) {
+    return state
+}
+
+export default allPhotosReducer
