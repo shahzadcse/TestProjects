d56ae6516deb3f5e675adbe6570f1f7242582e8a
Shahzad Hussain <shahzad.hussain@informa.com>
2020-05-31 23:47:11 +0400

    Action - update the reducer with RemovePhoto function logic

--- photo-project/src/redux/reducer.js
+++ photo-project/src/redux/reducer.js
@@ -1,7 +1,12 @@
 import allPhotos from '../data/allPhotos'
 
 const allPhotosReducer = function(state = allPhotos , action) {
-    console.log(action.index)
+    
+    switch(action.type) {
+        case 'REMOVE_PHOTO' : return [...state.slice(0, action.index), ...state.slice(action.index + 1)]
+        default: return state
+    }
+ 
     return state
 }
 
