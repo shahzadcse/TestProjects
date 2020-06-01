f6dd459a403a78ce825801713d1998d4a98c5e7d
Shahzad Hussain <shahzad.hussain@informa.com>
2020-06-01 18:41:40 +0400

    Adding Firebase- setup

--- photo-project/src/index.js
+++ photo-project/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom'
 // import Main from './components/Main' 
 import './styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
 import rootReducer from './redux/reducer'
 import {Provider} from 'react-redux'
 import App from './components/App'
+import {database} from './database/config'
 
-const store =  createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
+const store =  createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk) )
 
 
 ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'))
